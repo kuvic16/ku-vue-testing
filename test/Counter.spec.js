3f90cc10de92d5f7de19eb324bb0b1a42814a185
Shaiful Islam <kuvic16@gmail.com>
2020-04-24 02:03:44 +0600

    setting data and asserting styles

--- test/Counter.spec.js
+++ test/Counter.spec.js
@@ -14,13 +14,42 @@ describe("Counter", () => {
 
   it("increments the count when the button is clicked", () => {
     expect(wrapper.vm.count).toBe(0);
-    wrapper.find("button").trigger("click");
+    wrapper.find(".increment").trigger("click");
     expect(wrapper.vm.count).toBe(1);
+  });
+
+  it("decrements the count when the button is clicked", () => {
+    expect(wrapper.vm.count).toBe(0);
+    wrapper.find(".increment").trigger("click");
+    wrapper.find(".increment").trigger("click");
+
+    wrapper.find(".decrement").trigger("click");
+
+    expect(wrapper.vm.count).toBe(1);
+  });
+
+  it("another way: decrements the count when the button is clicked", () => {
+    wrapper.setData({count: 5})
+    wrapper.find(".decrement").trigger("click");
+    expect(wrapper.vm.count).toBe(4);
+  });
+
+  it('never goes below 0', () =>{
+    expect(wrapper.vm.count).toBe(0);
+    wrapper.find(".decrement").trigger("click");
+    expect(wrapper.vm.count).toBe(0);
+  });
+
+  it.only('decrement button visible when count is greater than 0', () =>{
+    expect(wrapper.vm.count).toBe(0);
+    expect(wrapper.find(".decrement").hasStyle("display", "none")).toBe(true);
+    wrapper.find(".increment").trigger("click");
+    expect(wrapper.find(".decrement").hasStyle("display", "none")).toBe(false);
   });
 
   it("presents the current count", () => {
     expect(wrapper.find(".count").html()).toContain(0);
-    wrapper.find("button").trigger("click");
+    wrapper.find(".increment").trigger("click");
     expect(wrapper.find(".count").html()).toContain(1);
   });
 });
