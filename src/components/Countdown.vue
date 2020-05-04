abecb976239cd0d8d9868298dd7619c6c366bd07
Shaiful Islam <kuvic16@gmail.com>
2020-05-04 17:08:33 +0600

    refactoring

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div v-if="!completed">
-      <span>{{ remaining.days }} Days,</span>
-      <span>{{ remaining.hours }} Hours,</span>
-      <span>{{ remaining.minutes }} Minutes,</span>
-      <span>{{ remaining.seconds }} Seconds,</span>
-      <!-- left... -->
+    <div v-if="!(remaining <= 0)">
+      <span>{{ remaining.days() }} Days,</span>
+      <span>{{ remaining.hours() }} Hours,</span>
+      <span>{{ remaining.minutes() }} Minutes,</span>
+      <span>{{ remaining.seconds() }} Seconds,</span>
+      left...
     </div>
     <div v-else v-text="expiredText"></div>
   </div>
@@ -25,21 +25,12 @@ export default {
     };
   },
   computed: {
-    completed() {
-      return this.remaining.total <= 0;
-    },
     remaining() {
       let remaining = moment.duration(Date.parse(this.until) - this.now);
       if (remaining <= 0) {
         this.$emit("finished");
       }
-      return {
-        total: remaining,
-        days: remaining.days(),
-        hours: remaining.hours(),
-        minutes: remaining.minutes(),
-        seconds: remaining.seconds(),
-      };
+      return remaining;
     },
   },
   created() {
--- test/Countdown.spec.js
+++ test/Countdown.spec.js
@@ -9,15 +9,17 @@ describe("Countdown", () => {
 
   beforeEach(() => {
     clock = sinon.useFakeTimers();
-    wrapper = mount(Countdown, () => {});
+    wrapper = mount(Countdown, {
+      propsData: {
+        until: moment().add(10, "seconds"),
+      },
+    });
   });
 
   afterEach(() => clock.restore());
 
   it("renders a countdown timer", () => {
     // <Countdown until="May 5 2020">
-    wrapper.setProps({ until: moment().add(10, "seconds") });
-
     see("0 Days");
     see("0 Hours");
     see("0 Minutes");
@@ -25,79 +27,48 @@ describe("Countdown", () => {
   });
 
   it("reduces the countdown every second", (done) => {
-    wrapper.setProps({ until: moment().add(10, "seconds") });
-
     see("10 Seconds");
     clock.tick(1000);
-    wrapper.vm.$nextTick(() => {
-      try {
-        see("9 Seconds");
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
+    assertOnNextTick(() => {
+      see("9 Seconds");
+    }, done);
   });
 
   it("shows an expired message when the countdown has expired", (done) => {
-    wrapper.setProps({ until: moment().add(10, "seconds") });
     clock.tick(10000);
-    wrapper.vm.$nextTick(() => {
-      try {
-        see("Now Expired");
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
+    assertOnNextTick(() => {
+      see("Now Expired");
+    }, done);
   });
 
   it("shows a custom expired message when the countdown has completed", (done) => {
     wrapper.setProps({
-      until: moment().add(10, "seconds"),
       expiredText: "Contest is over.",
     });
     clock.tick(10000);
-    wrapper.vm.$nextTick(() => {
-      try {
-        see("Contest is over.");
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
+    assertOnNextTick(() => {
+      see("Contest is over.");
+    }, done);
   });
 
   // <Countdown @finished="method">
   if (
     ("broadcasts when the countdown is finished",
     (done) => {
-      wrapper.setProps({ until: moment().add(10, "seconds") });
       clock.tick(10000);
-      wrapper.vm.$nextTick(() => {
-        try {
-          expect(wrapper.emitted().finished).toBeTruthy();
-          done();
-        } catch (e) {
-          done(e);
-        }
-      });
+      assertOnNextTick(() => {
+        clock.tick(5000);
+        expect(wrapper.emitted().finished).toBeTruthy();
+      }, done);
     })
   );
 
   it("clears the internal once completed", (done) => {
-    wrapper.setProps({ until: moment().add(10, "seconds") });
     clock.tick(10000);
     expect(wrapper.vm.now.getSeconds()).toBe(10);
-    wrapper.vm.$nextTick(() => {
-      clock.tick(5000);
-      try {
-        expect(wrapper.vm.now.getSeconds()).toBe(10);
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
+    assertOnNextTick(() => {
+      expect(wrapper.vm.now.getSeconds()).toBe(10);
+    }, done);
   });
 
   // helper function
@@ -115,5 +86,16 @@ describe("Countdown", () => {
 
     node.element.value = text;
     node.trigger("input");
+  };
+
+  let assertOnNextTick = (callback, done) => {
+    wrapper.vm.$nextTick(() => {
+      try {
+        callback();
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
   };
 });
